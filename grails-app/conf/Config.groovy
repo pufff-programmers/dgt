b507b7b38733b6afbbd2e32880853b0af7f19478
= <mariomgal@gmail.com>
2011-05-07 00:50:56 +0200

    Modelo básico de usuario, página y controlador de login generados con spring-security-core plugin

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -88,3 +88,8 @@ log4j = {
 
     warn   'org.mortbay.log'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.pufff.domain.user.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.pufff.domain.user.UserRole'
+grails.plugins.springsecurity.authority.className = 'com.pufff.domain.user.Role'
---
+++ grails-app/domain/com/pufff/domain/user/User.groovy
@@ -0,0 +1,24 @@
+package com.pufff.domain.user
+
+class User {
+
+	String username
+	String password
+	boolean enabled
+	boolean accountExpired
+	boolean accountLocked
+	boolean passwordExpired
+
+	static constraints = {
+		username email:true, blank: false, unique: true
+		password blank: false
+	}
+
+	static mapping = {
+		password column: '`password`'
+	}
+
+	Set<Role> getAuthorities() {
+		UserRole.findAllByUser(this).collect { it.role } as Set
+	}
+}
